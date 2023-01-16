c1ff34615764ad3926dffd3144d498aeec9b00e3
Andrew <abunyea@gmail.com>
2023-01-16 08:29:29 -0500

    Express routing to serve client assets from build dir

--- app/app.js
+++ app/app.js
@@ -1,6 +1,12 @@
 const express = require('express')
+const path = require('path');
 const app = express()
 const port = 8080
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use(express.static(path.join(__dirname, '../client/build')));
+
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+});
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
